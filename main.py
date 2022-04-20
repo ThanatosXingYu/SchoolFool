5f637a6845731e168f522bbf27302e305cec3156
Thanatos星宇 <53430376+ThanatosXingYu@users.noreply.github.com>
2022-04-20 10:25:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+from shutil import copytree
+from psutil import disk_partitions
+from tkinter import messagebox
+import webbrowser
+import os
+
+a=0
+t=0
+
+while a==0:
+    sleep(1)
+    for item in disk_partitions():
+        if 'removable' in item.opts:
+            #messagebox.showwarning(title="垃圾学校",message="我要放假")
+            #os.system("start D:\\HugoMoveData\\User\\seewo\\Downloads\\install\\a.vbs B MIN")
+            if t==0:
+                for i in range(4):
+                    webbrowser.open("fhzx.top/laji/a.mp3")
+                t=1
+            webbrowser.open("fhzx.top/laji")
+
+
